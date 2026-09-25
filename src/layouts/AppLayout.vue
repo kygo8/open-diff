863aea6b73e758bfc4d55481aecc92e6bbef223b
Yukon <126093594+baseGame@users.noreply.github.com>
2026-09-26 00:55:53 +0800

    Pin Picture session status mid column to capture 392.5px

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -2992,7 +2992,7 @@ const sourceSessionTypes = new Set<SessionType>([
 }
 
 .status-bar[data-chrome-kind='picture-session'] {
-  grid-template-columns: 294px minmax(0, 391.5px) minmax(0, 1fr);
+  grid-template-columns: 294px minmax(0, 392.5px) minmax(0, 1fr);
 }
 
 .status-bar[data-chrome-kind='media-session'] {

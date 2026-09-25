073a651c11110e05846121b6c8259dca707d3b04
Yukon <126093594+baseGame@users.noreply.github.com>
2026-09-26 00:56:04 +0800

    Pin Media session status mid column to capture 392.5px

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -2996,7 +2996,7 @@ const sourceSessionTypes = new Set<SessionType>([
 }
 
 .status-bar[data-chrome-kind='media-session'] {
-  grid-template-columns: 294px minmax(0, 391.5px) minmax(0, 1fr);
+  grid-template-columns: 294px minmax(0, 392.5px) minmax(0, 1fr);
 }
 
 .status-bar[data-chrome-kind='version-session'] {
